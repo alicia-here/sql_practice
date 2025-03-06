25439a0910a26d575f9f3ff5f67db568f8ddb235
Alicia Jung <jung.alicia03@gmail.com>
2025-03-06 20:41:17 +0900

    250306_Add_Quiz Lv.3 IS NULL / Lv.3 GROUP BY

--- sql_programmers/Level3.sql
+++ sql_programmers/Level3.sql
@@ -17,3 +17,51 @@ GROUP BY CATEGORY
 ORDER BY CATEGORY ASC;
 
 ------------------------------------------------------------------------------------------------------
+
+## 2. 업그레이드 할 수 없는 아이템 구하기 (Lv.3 / IS NULL)
+
+WITH FIND_NEXT AS (SELECT 
+                   II.ITEM_ID,
+                   II.ITEM_NAME,
+                   II.RARITY,
+                   IT.PARENT_ITEM_ID,
+                   IT.ITEM_ID AS NEXT
+                   FROM ITEM_INFO II
+                   LEFT JOIN ITEM_TREE IT
+                   ON II.ITEM_ID = IT.PARENT_ITEM_ID)
+SELECT 
+    ITEM_ID,
+    ITEM_NAME,
+    RARITY
+FROM 
+    FIND_NEXT
+WHERE
+    NEXT IS NULL
+ORDER BY
+    ITEM_ID DESC;
+
+------------------------------------------------------------------------------------------------------
+
+## 3. 부서별 연봉 조회하기 (Lv.3 / GROUP BY)
+
+WITH CHECK_SALARY AS (SELECT 
+                        HE.EMP_NO,
+                        HE.DEPT_ID,
+                        HD.DEPT_NAME_EN,
+                        HE.SAL
+                      FROM
+                        HR_EMPLOYEES HE
+                      LEFT JOIN 
+                        HR_DEPARTMENT HD
+                      ON 
+                        HE.DEPT_ID = HD.DEPT_ID)
+SELECT 
+    DEPT_ID, 
+    DEPT_NAME_EN,
+    ROUND(AVG(SAL)) AS AVG_SAL
+FROM 
+    CHECK_SALARY
+GROUP BY
+    DEPT_ID
+ORDER BY 
+    AVG_SAL DESC;
